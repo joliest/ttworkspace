e2808d6ccca4e55a234a2ba03b220aaea2a3ffb4
Joliver Estampador <joliverestampador@gmail.com>
2018-12-08 14:11:02 +0800

    COmpleted Vue and Node test

---
+++ client/src/components/Register.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="text" 
+           placeholder="Email"
+           v-model="email"> <br>
+    <input type="text"
+           placeholder="Password" 
+          v-model="password"> <br>
+    <button type="button" @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationServices from '@/services/AuthenticationServices'
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register() {
+    const response = await AuthenticationServices.register({
+      email: this.email,
+      password: this.password
+    });
+    console.log(response.data);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
